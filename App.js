9c8797f4f2e27a01412a8ade127f7e2cf3ccc8ad
unknown <fiiarkhurst@gmail.com>
2020-04-11 21:21:14 +0100

    changed map style to look like ubers UI

--- App.js
+++ App.js
@@ -5,6 +5,10 @@ import Constants from 'expo-constants';
 import * as Location from 'expo-location';
 import { Ionicons } from '@expo/vector-icons';
 
+import { mapStyle} from './utils/MapStyle';
+import Interaction from './components/Interaction';
+
+
 export default function App() {
   const [ location, setLocation ] = React.useState(null);
   const [ errorMsg, setErromsg] = React.useState(null);
@@ -13,6 +17,7 @@ export default function App() {
     _getLocation();
   });
 
+  // Get Location
   const _getLocation  = async () => {
     let {status} = await Location.requestPermissionsAsync();
     if(status !== "granted"){
@@ -25,166 +30,6 @@ export default function App() {
 
   }
 
-  const mapStyle = [
-    {
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#f5f5f5"
-        }
-      ]
-    },
-    {
-      "elementType": "labels.icon",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
-    },
-    {
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#616161"
-        }
-      ]
-    },
-    {
-      "elementType": "labels.text.stroke",
-      "stylers": [
-        {
-          "color": "#f5f5f5"
-        }
-      ]
-    },
-    {
-      "featureType": "administrative.land_parcel",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#bdbdbd"
-        }
-      ]
-    },
-    {
-      "featureType": "poi",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#eeeeee"
-        }
-      ]
-    },
-    {
-      "featureType": "poi",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#757575"
-        }
-      ]
-    },
-    {
-      "featureType": "poi.park",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#e5e5e5"
-        }
-      ]
-    },
-    {
-      "featureType": "poi.park",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#9e9e9e"
-        }
-      ]
-    },
-    {
-      "featureType": "road",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#ffffff"
-        }
-      ]
-    },
-    {
-      "featureType": "road.arterial",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#757575"
-        }
-      ]
-    },
-    {
-      "featureType": "road.highway",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#dadada"
-        }
-      ]
-    },
-    {
-      "featureType": "road.highway",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#616161"
-        }
-      ]
-    },
-    {
-      "featureType": "road.local",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#9e9e9e"
-        }
-      ]
-    },
-    {
-      "featureType": "transit.line",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#e5e5e5"
-        }
-      ]
-    },
-    {
-      "featureType": "transit.station",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#eeeeee"
-        }
-      ]
-    },
-    {
-      "featureType": "water",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "color": "#c9c9c9"
-        }
-      ]
-    },
-    {
-      "featureType": "water",
-      "elementType": "labels.text.fill",
-      "stylers": [
-        {
-          "color": "#9e9e9e"
-        }
-      ]
-    }
-  ]
 
   if(errorMsg !==null){
     return (
@@ -193,25 +38,20 @@ export default function App() {
   }
   return (
     <React.Fragment>
-      <MapView style={styles.map}
-      customMapStyle={mapStyle}
-      provider={MapView.PROVIDER_GOOGLE}
-      initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922/2.5,
-        longitudeDelta: 0.0421/2.5
-      }}
-    />
-    <View
-    style={styles.interaction}>
-      <View style={styles.user}>
-        <Text style={styles.greeting}>Welcome Fiifi</Text>
-      </View>
-    </View>
-    <View style={styles.menu}>
-      <Ionicons style={{marginTop:4}} name="ios-menu" size={27} />
-    </View>
+        <MapView style={styles.map}
+          customMapStyle={mapStyle}
+          provider={MapView.PROVIDER_GOOGLE}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922/2.5,
+            longitudeDelta: 0.0421/2.5
+          }}
+        />
+        <Interaction />
+        <View style={styles.menu}>
+          <Ionicons style={{marginTop:4}} name="ios-menu" size={27} />
+        </View>
     </React.Fragment>
   );
 }
@@ -221,24 +61,6 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: Constants.statusBarHeight,
 
-  },
-  interaction:{
-    height:340,
-    backgroundColor:"whitesmoke", 
-    shadowColor: '#000',
-    shadowOpacity: 0.1,
-    shadowOffset: { width: 0, height: 1 },
-  },
-  user:{
-    borderBottomWidth: 1.5,
-    justifyContent:'center',
-    alignItems:'center' ,
-    paddingVertical:20,
-    borderColor:'#cecece'
-  },
-  greeting:{
-    fontWeight:'600',
-    fontSize:16
   },
   menu:{
     position:'absolute',
---
+++ components/Interaction.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+
+const Interaction  = () => {
+    return(
+        <View style={styles.container}>
+           <View style={styles.user}>
+             <Text style={styles.greeting}>Good morning, Fiifi</Text>
+            </View>
+        </View>    
+    )
+};
+
+const styles = StyleSheet.create({
+    container : {
+        height:340,
+        backgroundColor:"whitesmoke", 
+        shadowColor: '#000',
+        shadowOpacity: 0.1,
+        shadowOffset: { width: 0, height: 1 },
+    },
+    user:{
+        borderBottomWidth: 1.5,
+        justifyContent:'center',
+        alignItems:'center' ,
+        paddingVertical:20,
+        borderColor:'#cecece'
+      },
+      greeting:{
+        fontWeight:'600',
+        fontSize:16
+      },
+});
+
+export default Interaction;
