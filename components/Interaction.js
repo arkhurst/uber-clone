f0ddd79d4b0a2614e1ff7d878fc54c4260c4b406
unknown <fiiarkhurst@gmail.com>
2020-04-15 01:16:22 +0100

    set up modal

--- components/Interaction.js
+++ components/Interaction.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { View, StyleSheet, Text, KeyboardAvoidingView } from 'react-native';
 import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 import {Ionicons} from '@expo/vector-icons';
+import ScheduleModal from './ScheduleModal';
 
 
 const Interaction  = ({navigation}) => {
+
+  const [visible, setVisible] = React.useState(false);
+
+  const cancelModal = () => {
+    setVisible(false);
+  }
+
+  const openModal = () => {
+    setVisible(true)
+  }
     return(
         <View style={styles.container}>
            <View style={styles.user}>
@@ -14,10 +25,11 @@ const Interaction  = ({navigation}) => {
               <TouchableOpacity  onPress={() => navigation.navigate('Search')}>
                   <Text style={{fontSize:19, fontWeight:'bold'}}>Where to</Text>
               </TouchableOpacity>
-              <TouchableOpacity style={styles.schedule}>
+              <TouchableOpacity onPress={() => openModal()} style={styles.schedule}>
                 <Ionicons name="ios-time" size={18} />
                 <Text style={{fontWeight:'bold'}}>Now</Text>
                 <Ionicons name="ios-arrow-down" size={18} />
+               <ScheduleModal visible={visible} cancelModal={() => cancelModal()} />
               </TouchableOpacity>
            </View>
         </View>    
--- components/ScheduleModal.js
+++ components/ScheduleModal.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { Text, View, Modal } from 'react-native';
+import { Text, View, Modal, Button, StyleSheet } from 'react-native';
 
 
-const ScheduleModal = () => {
+const ScheduleModal = ({visible, cancelModal}) => {
     return(
-        <Modal>
+        <Modal transparent={true} visible={visible}  animationType="slide">
+            <View style={styles.modalView}>
+            <Button title="close" onPress={cancelModal} />
             <Text>Hey</Text>
+            
+            </View>
         </Modal>
     );
 };
 
+const styles = StyleSheet.create({
+    modalView: {
+        marginTop: 280,
+        backgroundColor: "white",
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+          width: 0,
+          height: 1
+        },
+        shadowOpacity: 0.25,
+        elevation: 5
+      },
+})
 export default ScheduleModal;
