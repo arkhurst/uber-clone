789341c1fddaf4c293476329c466feb0e9152d0e
arkhurst <fiiarkurst@gmail.com>
2020-04-25 04:18:14 -0700

    fixed bugs

--- Main.js
+++ Main.js
@@ -1,25 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, StyleSheet, View, TouchableOpacity } from 'react-native';
 import MapView from 'react-native-maps';
 import Constants from 'expo-constants';
 import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 import { Ionicons } from '@expo/vector-icons';
 
 import { mapStyle} from './utils/MapStyle';
 import Interaction from './components/Interaction';
+import Device from './utils/Device';
 
 
 export default function Main({navigation}) {
-  const [ location, setLocation ] = React.useState({
-     region:{
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-     }
-  });
-  const [ errorMsg, setErromsg] = React.useState(null);
+  const [showMap, setShowMap] = useState(false);
+  const [ userLat, setUserLat ] = useState(null);
+  const [ userLon, setUserLon ] = useState(null);
+
 
   React.useEffect(() => {
     _getLocation();
@@ -27,44 +24,54 @@ export default function Main({navigation}) {
 
   // Get Location
   const _getLocation  = async () => {
-    let {status} = await Location.requestPermissionsAsync();
-    if(status !== "granted"){
-      setErromsg('User did not allow location');
-      return;
+    let { status: existingStatus } = await Permissions.getAsync(
+      Permissions.LOCATION
+    )
+    let finalStatus = existingStatus;
+
+    // ask user for permission again if it is not granted
+    if(existingStatus !== 'granted'){
+      const { status } = await Permissions.askAsync(Permissions.LOCATION);
+      finalStatus = status;
     }
 
-    let location = await Location.getCurrentPositionAsync({});
-    // let location2 = await Location.reverseGeocodeAsync(location.coords);
-    // console.log(location2)
+    // user still doesn't allow permission?
+    if(finalStatus !== 'granted'){
+      return;
+    }
+ 
+
+    const { coords } = await Location.getCurrentPositionAsync();
   
-    setLocation({
-      region: {
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421
-      }
-    })
+    setShowMap(true)
+    setUserLat(coords.latitude)
+    setUserLon(coords.longitude)
 
   }
 
-
-  if(errorMsg !==null){
-    return (
-      <Text>{errorMsg}</Text>
-    )
-  }
+ if(!showMap){
+   <View style={styles.NoLocationContainer}>
+      <Text style={styles.textLocationNeeded}>
+        We need your location data ...
+      </Text>
+   </View>
+ } 
   return (
     <React.Fragment>
         <MapView style={styles.map}
           customMapStyle={mapStyle}
+          followsUserLocation
           provider={MapView.PROVIDER_GOOGLE}
           showsUserLocation
           showsBuildings
           shadowOpacity
           showsPointsOfInterest
-          initialRegion={location.region}
-          // region={location.region}
+          region={{
+            latitude:userLat,
+            longitude:userLon,
+            latitudeDelta:0.01,
+            longitudeDelta:0.01
+          }}
        
         />
         <Interaction navigation={navigation} />
@@ -94,5 +101,16 @@ const styles = StyleSheet.create({
     borderRadius:25,
     justifyContent:'center',
     alignItems:'center'
+  },
+  NoLocationContainer:{
+    justifyContent:'center',
+    alignItems:'center',
+    height: Device.height,
+    width: Device.width,
+    position:'absolute'
+  },
+  textLocationNeeded:{
+    fontSize:16,
+    marginBottom:16
   }
 });
