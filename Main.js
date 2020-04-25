141da5140140f993b971702d33f4e1bff1533133
unknown <fiiarkhurst@gmail.com>
2020-04-25 12:59:35 +0100

    fixed map issues

--- Main.js
+++ Main.js
@@ -12,88 +12,113 @@ import Interaction from './components/Interaction';
 import Device from './utils/Device';
 
 
-export default function Main({navigation}) {
-  const [showMap, setShowMap] = useState(false);
-  const [ userLat, setUserLat ] = useState(null);
-  const [ userLon, setUserLon ] = useState(null);
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      showMap: false,
+      userLat: null,
+      userLon: null
+    };
 
-  React.useEffect(() => {
-    _getLocation();
-  });
+  }
 
-  // Get Location
-  const _getLocation  = async () => {
-    let { status: existingStatus } = await Permissions.getAsync(
+  async componentDidMount() {
+    // get exisiting locaton permissions first
+    const { status: existingStatus } = await Permissions.getAsync(
       Permissions.LOCATION
-    )
+    );
     let finalStatus = existingStatus;
 
-    // ask user for permission again if it is not granted
-    if(existingStatus !== 'granted'){
+    // ask again to grant locaton permissions (if not already allowed)
+    if (existingStatus !== 'granted') {
       const { status } = await Permissions.askAsync(Permissions.LOCATION);
       finalStatus = status;
     }
 
-    // user still doesn't allow permission?
-    if(finalStatus !== 'granted'){
+    // still not allowed to use location?
+    if (finalStatus !== 'granted') {
       return;
     }
- 
 
     const { coords } = await Location.getCurrentPositionAsync();
-  
-    setShowMap(true)
-    setUserLat(coords.latitude)
-    setUserLon(coords.longitude)
 
+    this.setState({
+      showMap: true,
+      userLat: coords.latitude,
+      userLon: coords.longitude
+    });
   }
 
- if(!showMap){
-   <View style={styles.NoLocationContainer}>
-      <Text style={styles.textLocationNeeded}>
-        We need your location data ...
-      </Text>
-   </View>
- } 
-  return (
-    <React.Fragment>
-        <MapView style={styles.map}
-          customMapStyle={mapStyle}
-          followsUserLocation
-          provider={MapView.PROVIDER_GOOGLE}
-          showsUserLocation
-          showsBuildings
-          shadowOpacity
-          showsPointsOfInterest
-          region={{
-            latitude:userLat,
-            longitude:userLon,
-            latitudeDelta:0.01,
-            longitudeDelta:0.01
-          }}
-       
-        />
-        <Interaction navigation={navigation} />
-        <TouchableOpacity onPress={() => navigation.openDrawer()} style={styles.menu}>
-          <Ionicons style={{marginTop:4}} name="ios-menu" size={27} />
-        </TouchableOpacity>
-    </React.Fragment>
-  );
+
+  render() {
+    const { navigation } = this.props;
+    const {  showMap, userLat, userLon } = this.state;
+
+    return (
+      <React.Fragment>
+        {showMap && (
+         <React.Fragment>
+          <MapView style={styles.map}
+            followsUserLocation
+            provider={MapView.PROVIDER_GOOGLE}
+            region={{
+              latitude: userLat,
+              longitude: userLon,
+              latitudeDelta: 0.01,
+              longitudeDelta: 0.01
+            }}
+            showsUserLocation
+            customMapStyle={mapStyle}
+            showsBuildings
+            shadowOpacity
+            showsPointsOfInterest
+          />
+             <Interaction navigation={navigation} />
+             <TouchableOpacity style={styles.menu} onPress={() => navigation.toggleDrawer()}>
+                 <Ionicons style={{marginTop:4}} name="ios-menu" size={27} />
+             </TouchableOpacity>
+
+     </React.Fragment>
+          
+        )}
+
+        {!showMap && (
+          <View style={styles.containerNoLocation}>
+            <Text style={styles.textLocationNeeded}>
+            {"Picking up your location\n We need your location data..."}
+            </Text>
+          </View>
+        )}
+       </React.Fragment>
+    );
+  }
 }
+
 
 const styles = StyleSheet.create({
   map:{
-    flex: 1,
-    paddingTop: Constants.statusBarHeight,
-
+    flex:1,
+    paddingTop:Constants.statusBarHeight
+  },
+  containerNoLocation: {
+    alignItems: 'center',
+    height: Device.height,
+    justifyContent: 'center',
+    position: 'absolute',
+    width: Device.width
+  },
+  textLocationNeeded: {
+    fontSize: 16,
+    marginBottom: 16
   },
   menu:{
     position:'absolute',
-    backgroundColor: 'white',
-    shadowColor: '#000',
-    shadowOpacity: 0.1,
-    shadowOffset: { width: 0, height: 1 },
+    backgroundColor:"white",
+    shadowColor:'#000',
+    shadowOffset:0.1,
+    shadowOffset: { width:0, height:1},
     height:45,
     width:45,
     left:20,
@@ -101,16 +126,9 @@ const styles = StyleSheet.create({
     borderRadius:25,
     justifyContent:'center',
     alignItems:'center'
-  },
-  NoLocationContainer:{
-    justifyContent:'center',
-    alignItems:'center',
-    height: Device.height,
-    width: Device.width,
-    position:'absolute'
-  },
-  textLocationNeeded:{
-    fontSize:16,
-    marginBottom:16
   }
+
 });
+
+export default Main;
+
